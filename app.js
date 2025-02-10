ccfffdf1e364e0c667f8bc2a3ecb826abfefe70f
savyasachi bhat <savyasachibhat52@gmail.com>
2025-02-10 13:32:11 +0530

    added

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal Server Error' });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT  || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
