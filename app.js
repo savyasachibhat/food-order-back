f21a9c125bb2802a4184bf2d0974918711af9cf2
savyasachi bhat <savyasachibhat52@gmail.com>
2025-02-12 12:20:09 +0530

    new

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Always serve index.html for all routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 // Routes
 app.get("/", (req,res)=>{
   res.send("Welcome to the Food Order API!");
