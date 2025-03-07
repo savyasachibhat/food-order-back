e68b34355bf0d23ade46a4d808c8392993153b0a
savyasachi bhat <savyasachibhat52@gmail.com>
2025-03-07 18:27:58 +0530

    new

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const userRoutes = require('./routes/user');
 // Database connection
 const { connectDB } = require('./config/db');
 connectDB();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Change "*" to your frontend domain for production
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
